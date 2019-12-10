d2d45aa8e7afc671f17ec94f12d1fbb1d98c9764
Kirill Sofranciuc <luperkalfan@gmail.com>
2019-12-10 12:13:16 +0300

    add solution from local folder

---
+++ GeneratorApp/SequenceGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //Dictionary//
+			const string dict = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            //Sequence length//
+			const int n = 20;
+			for (BigInteger i = 0; i < (BigInteger)(Math.Pow(dict.Length, n)); i++)
+			{
+				StringBuilder pass = new StringBuilder();
+				for (int j = n-1; j>=0; j--)
+				{
+					pass.Append(dict[(int) ((i / (BigInteger) Math.Pow(dict.Length, j)) % dict.Length)]);
+				}
+				Console.WriteLine(pass);
+			}
+			Console.ReadKey();
+		}
+
+		
+	}
+}
